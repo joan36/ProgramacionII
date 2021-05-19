9a7980ca013a886899fd8960963b0a60d1ec7360
joan36 <gomilajoan36@gmail.com>
2021-05-19 07:23:27 +0200

    c

--- Joc del laberint/src/Fitxers.java
+++ Joc del laberint/src/Fitxers.java
@@ -4,37 +4,44 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Fitxers {
-    static void lecturaLaberint(){
-        int files;
-        int columnes;
+    static char[][] lecturaLaberint() {
+        int files, columnes, filaSortida, columnesSortida;
         final int longExacta = 4;
-        final int fin = 19;
-        int[][] matriu=new int[19][4];
-        int i =0;
+        final int fin = 15;
+        char[][] matriu = new char[150][4];
+        String numero = "";
+        int i = 0;
 
         try {
             File arxiu = new File("Laberints/maze1.txt");
-            RandomAccessFile f=new RandomAccessFile(arxiu,"r");
-            long numreg = f.length() / longExacta;
+            RandomAccessFile f = new RandomAccessFile(arxiu, "r");
 
-            files= Integer.parseInt(f.readLine());
-            columnes= Integer.parseInt(f.readLine());
-            while (f.readChar()!=0 && f.readChar()!=1){
-                for (int j = 0; j < longExacta; j++) {
-                    matriu[i][j]=f.readChar();
+            files = Integer.parseInt(f.readLine());
+            columnes = Integer.parseInt(f.readLine());
+
+            for (int j = 0; j < fin; j++) {
+                numero += f.readLine();
+
+            }
+            char[] n = numero.toCharArray();
+
+            while (i != n.length) {
+                for (int j = 0; j < 150; j++) {
+                    for (int k = 0; k < 4; k++) {
+                        matriu[j][k] = n[i];
+                        i++;
+                    }
                 }
-                i++;
             }
 
-            
-
-
+            filaSortida = Integer.parseInt(f.readLine());
+            columnesSortida = Integer.parseInt(f.readLine());
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        return matriu;
     }
 }
--- Joc del laberint/src/Laberint.java
+++ Joc del laberint/src/Laberint.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
 
 public class Laberint extends JFrame implements ActionListener {
     private JTextField l;
+    private static final int files =15;
+    private static final int columna =10;
 
 
     public Laberint(){
@@ -11,16 +15,27 @@ public class Laberint extends JFrame implements ActionListener {
         setSize(800,900);
         setLocationRelativeTo(null);//El laberint surt al centre
         setDefaultCloseOperation(Laberint.EXIT_ON_CLOSE);
-        initContents();
+        pintaLaberint();
+
     }
 
-    private void initContents(){
+    private void pintaLaberint(){
+        char[][] laberinto = Fitxers.lecturaLaberint();
+        int y=0;
+
+        for (int i = 0; i < files; i++) {
+            int x=0;
+            for (int j = 0; j < columna; j++) {
+                Rectangle2D.Float r = new Rectangle2D.Float(x,y,40,40);
+
+            }
+        }
 
     }
 
     public static void main(String[] args) {
-        //new Laberint().setVisible(true);
-        Fitxers.lecturaLaberint();
+        new Laberint().setVisible(true);
+        //Fitxers.lecturaLaberint();
     }
 
     @Override
